b94d13a6d311e38a764776e545175d889e55b2f4
Unknown <201403907@ingenieria.usac.edu.gt>
2016-12-07 00:11:44 -0600

    Interfaz gráfica
    La interfaz de la aplicación se encuentra realizada, ninguna funcionalidad se ha realizado.

---
+++ HT1/HT1/HT1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace HT1
+{
+    partial class HT1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.entradaa = new System.Windows.Forms.TextBox();
+            this.entradab = new System.Windows.Forms.TextBox();
+            this.entradac = new System.Windows.Forms.TextBox();
+            this.entradad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.respuesta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Variable a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Variable b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Variable c";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Variable d";
+            // 
+            // entradaa
+            // 
+            this.entradaa.Location = new System.Drawing.Point(125, 30);
+            this.entradaa.Name = "entradaa";
+            this.entradaa.Size = new System.Drawing.Size(100, 20);
+            this.entradaa.TabIndex = 4;
+            // 
+            // entradab
+            // 
+            this.entradab.Location = new System.Drawing.Point(125, 60);
+            this.entradab.Name = "entradab";
+            this.entradab.Size = new System.Drawing.Size(100, 20);
+            this.entradab.TabIndex = 5;
+            // 
+            // entradac
+            // 
+            this.entradac.Location = new System.Drawing.Point(125, 90);
+            this.entradac.Name = "entradac";
+            this.entradac.Size = new System.Drawing.Size(100, 20);
+            this.entradac.TabIndex = 6;
+            // 
+            // entradad
+            // 
+            this.entradad.Location = new System.Drawing.Point(125, 121);
+            this.entradad.Name = "entradad";
+            this.entradad.Size = new System.Drawing.Size(100, 20);
+            this.entradad.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(150, 219);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Respuesta";
+            // 
+            // respuesta
+            // 
+            this.respuesta.Location = new System.Drawing.Point(125, 269);
+            this.respuesta.Name = "respuesta";
+            this.respuesta.Size = new System.Drawing.Size(100, 20);
+            this.respuesta.TabIndex = 13;
+            // 
+            // HT1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 318);
+            this.Controls.Add(this.respuesta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.entradad);
+            this.Controls.Add(this.entradac);
+            this.Controls.Add(this.entradab);
+            this.Controls.Add(this.entradaa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "HT1";
+            this.Text = "HT1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox entradaa;
+        private System.Windows.Forms.TextBox entradab;
+        private System.Windows.Forms.TextBox entradac;
+        private System.Windows.Forms.TextBox entradad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox respuesta;
+    }
+}
+
---
+++ HT1/HT1/HT1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HT1
+{
+    public partial class HT1 : Form
+    {
+        public HT1()
+        {
+            InitializeComponent();
+        }
+    }
+}
